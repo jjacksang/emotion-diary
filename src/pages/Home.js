8d4b1927f20813facd381d9aab181f90a952327d
Sangwoo <jswoo1995@gmail.com>
2023-12-30 22:04:01 +0900

    finish inflearn cose

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,12 @@ const Home = () => {
 
     const [data, setData] = useState([]);
     const [curDate, setCurDate] = useState(new Date());
-
     const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월 `;
+
+    useEffect(() => {
+        const titleElement = document.getElementsByTagName("title")[0];
+        titleElement.innerHTML = `감정 일기장`;
+    }, []);
 
     useEffect(() => {
         if (diaryList.length >= 1) {
